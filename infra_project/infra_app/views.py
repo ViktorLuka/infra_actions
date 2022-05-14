e965d4d9045da8e6d301685d79388a01c25b45dd
Виктор Лукьянчук <djrama23@gmail.com>
2022-05-14 23:13:28 +0300

    test

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse(request, 'У меня получилось!')
+    return HttpResponse('У меня получилось!', status_code=200)
 
 
 def second_page(request):
-    return HttpResponse(request, 'А это вторая страница')
+    return HttpResponse('А это вторая страница', status_code=200)
